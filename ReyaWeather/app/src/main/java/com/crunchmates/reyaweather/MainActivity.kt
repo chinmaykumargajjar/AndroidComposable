aed38bf47bca8eb655c411ff7f84fc811f153c35
Chinmaykumar Gajjar <gajjar.chinmay@gmail.com>
2024-09-25 13:39:53 -0400

    Connect Splash Screen
    Add Splash Screen as part of Main Activity and connect Navigation composable function over to main activity that in the end using and connecting controller.

--- ReyaWeather/app/src/main/java/com/crunchmates/reyaweather/MainActivity.kt
+++ ReyaWeather/app/src/main/java/com/crunchmates/reyaweather/MainActivity.kt
@@ -3,13 +3,20 @@ package com.crunchmates.reyaweather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.crunchmates.reyaweather.navigation.WeatherNavigation
+import com.crunchmates.reyaweather.navigation.WeatherScreens
 import com.crunchmates.reyaweather.ui.theme.ReyaWeatherTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -19,30 +26,30 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
         setContent {
-            ReyaWeatherTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            WeatherApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun WeatherApp() {
+    ReyaWeatherTheme {
+        Surface(
+            color = MaterialTheme.colors.background,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                WeatherNavigation()
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ReyaWeatherTheme {
-        Greeting("Android")
-    }
+    WeatherApp()
 }
