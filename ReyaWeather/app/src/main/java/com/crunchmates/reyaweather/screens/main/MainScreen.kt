7258c573ec367dd0785ca51ddd8af105e0ab3630
Chinmaykumar Gajjar <gajjar.chinmay@gmail.com>
2024-09-28 09:33:42 -0400

    Fix App Bar Padding
    Bring Toronto down and not overlap the app bar anymore

--- ReyaWeather/app/src/main/java/com/crunchmates/reyaweather/screens/main/MainScreen.kt
+++ ReyaWeather/app/src/main/java/com/crunchmates/reyaweather/screens/main/MainScreen.kt
@@ -1,21 +1,25 @@
 package com.crunchmates.reyaweather.screens.main
 
+import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.produceState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.crunchmates.reyaweather.data.DataOrException
 import com.crunchmates.reyaweather.model.Weather
+import com.crunchmates.reyaweather.widget.WeatherAppBar
 
 @Composable
 fun MainScreen(navController: NavController, mainViewModel: MainViewModel = hiltViewModel()) {
-    ShowData(mainViewModel)
-}
 
-@Composable
-fun ShowData(mainViewModel: MainViewModel) {
     val weatherData = produceState<DataOrException<Weather, Boolean, Exception>> (
         initialValue = DataOrException(loading = true)
     ) {
@@ -25,7 +29,30 @@ fun ShowData(mainViewModel: MainViewModel) {
     if (weatherData.loading == true) {
         CircularProgressIndicator()
     } else if (weatherData.data != null) {
-        Text(text = "Main Screen ${weatherData.data!!}")
+        MainScaffold(weather = weatherData.data!!, navController)
     }
-
 }
+@Composable
+fun MainScaffold(weather: Weather, navController: NavController) {
+    Scaffold(
+        topBar = {
+            WeatherAppBar(
+                title = "Helena, MT",
+                navController = navController,
+                elevation = 5.dp
+            )
+        }
+    ) { paddingValues ->  // `paddingValues` represents the padding that Scaffold applies.
+        MainContent(data = weather, paddingValues = paddingValues)
+    }
+}
+
+@Composable
+fun MainContent(data: Weather, paddingValues: PaddingValues) {
+    // Apply the padding from Scaffold to ensure the content doesn't get obscured.
+    Column(modifier = Modifier.padding(paddingValues)) {
+        Text(text = data.city.name)
+        // Add more content if needed
+    }
+}
+
---
+++ ReyaWeather/app/src/main/java/com/crunchmates/reyaweather/widget/WeatherAppBar.kt
@@ -0,0 +1,37 @@
+package com.crunchmates.reyaweather.widget
+
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+
+@Preview
+@Composable
+fun WeatherAppBar(
+    title: String = "Title",
+    icon: ImageVector? = null,
+    isMainScreen: Boolean = true,
+    elevation: Dp = 0.dp,
+    navController: NavController,
+    onAddActionClicked: () -> Unit = {},
+    onButtonClicked: () -> Unit = {}){
+
+    TopAppBar(
+        title = {
+            Text(text = title)
+        },
+        actions = {},
+        navigationIcon = {},
+        backgroundColor = Color.Transparent,
+        elevation = elevation)
+
+}
