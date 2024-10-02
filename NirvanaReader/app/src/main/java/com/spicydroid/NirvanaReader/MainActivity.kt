ad1d36d7eda0e6b66252cc5199b7e2d2725826f7
Chinmaykumar Gajjar <gajjar.chinmay@gmail.com>
2024-10-02 14:57:17 -0400

    Test Firestore database

--- NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/MainActivity.kt
+++ NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.spicydroid.NirvanaReader
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,12 +11,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.firestore.FirebaseFirestore
 import com.spicydroid.NirvanaReader.ui.theme.NirvanaReaderTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        val db = FirebaseFirestore.getInstance()
+        val user: MutableMap<String, Any> = HashMap()
+        user["firstName"] = "Jeo"
+        user["lastName"] = "James"
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener {
+                Log.d("TAG", "onCreate: $it.id")
+            }.addOnFailureListener{
+                Log.d("TAG", "onFailure: $it.id")
+            }
         setContent {
             NirvanaReaderTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
@@ -28,6 +40,7 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
 
 @Composable
