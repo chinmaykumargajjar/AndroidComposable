0792df5e42b9176944a970ec26d94fa73299ab12
Chinmaykumar Gajjar <gajjar.chinmay@gmail.com>
2024-10-02 16:07:38 -0400

    Setup Navigation
    Add NavHost to different classes

--- NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/navigation/ReaderNavigation.kt
+++ NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/navigation/ReaderNavigation.kt
@@ -1,4 +1,11 @@
 package com.spicydroid.NirvanaReader.navigation
 
-class ReaderNavigation {
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun ReaderNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = )
 }
