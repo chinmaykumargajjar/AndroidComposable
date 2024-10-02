3fd3f5f6ad2d5e410e0abd5f282e4e2bc076debb
Chinmaykumar Gajjar <gajjar.chinmay@gmail.com>
2024-10-02 15:04:13 -0400

    Basic Hilt Integration

---
+++ NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/ReaderApplication.kt
@@ -0,0 +1,9 @@
+package com.spicydroid.NirvanaReader
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ReaderApplication: Application() {
+    
+}
---
+++ NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/di/AppModule.kt
@@ -0,0 +1,4 @@
+package com.spicydroid.NirvanaReader.di
+
+object AppModule {
+}
