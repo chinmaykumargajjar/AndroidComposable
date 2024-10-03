d0f089bb8574fa114102b148d8503a42bf4165c5
Chinmaykumar Gajjar <gajjar.chinmay@gmail.com>
2024-10-03 13:10:36 -0400

    Reya Weather Video
    Splash Screen for Nirvana Reader

--- NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/screens/home/ReaderHomeScreen.kt
+++ NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/screens/home/ReaderHomeScreen.kt
@@ -1,9 +1,10 @@
 package com.spicydroid.NirvanaReader.screens.home
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 
 @Composable
 fun Home(navController: NavHostController) {
-
+    Text("Home")
 }
--- NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/screens/login/ReaderLoginScreen.kt
+++ NirvanaReader/app/src/main/java/com/spicydroid/NirvanaReader/screens/login/ReaderLoginScreen.kt
@@ -1,9 +1,108 @@
 package com.spicydroid.NirvanaReader.screens.login
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRestorer
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import coil.compose.ImagePainter
+import com.spicydroid.NirvanaReader.components.EmailInput
+import com.spicydroid.NirvanaReader.components.PasswordInput
+import com.spicydroid.NirvanaReader.screens.ReaderLogo
 
 @Composable
 fun ReaderLoginScreen(navController: NavHostController) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center){
+            ReaderLogo()
+            UserForm(loading = false, isCreateAccount = false){  email, password ->
+                Log.d("TAG", "ReaderLogin Screen: $email $password")
+            }
 
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
+@Composable
+fun UserForm(
+    loading : Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, pwd -> }
+) {
+    val email = rememberSaveable {
+        mutableStateOf("")
+    }
+    val password = rememberSaveable() {
+        mutableStateOf("")
+    }
+    val passwordVisibility = rememberSaveable {
+        mutableStateOf(false)
+    }
+    val passwordFocusRequest = FocusRequester.Default
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val valid = remember(email.value, password.value) {
+        email.value.trim().isNotEmpty() && password.value.trim().isNotEmpty()
+    }
+
+    val modifier =Modifier
+        .height(250.dp)
+        .background(MaterialTheme.colorScheme.background)
+        .verticalScroll(rememberScrollState())
+
+    Column(modifier,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        EmailInput(emailState = email, enabled = !loading,
+            onAction = KeyboardActions{
+                passwordFocusRequest.requestFocus()
+            })
+    }
+
+    PasswordInput(
+        modifier = Modifier.focusRestorer({ passwordFocusRequest }),
+        passwordState = password,
+        labelId = "Password",
+        enabled = !loading,
+        passwordVisibility = passwordVisibility,
+        onAction = KeyboardActions {
+            if(!valid) return@KeyboardActions
+            onDone(email.value.trim(), password.value.trim())
+        }
+    )
 }
